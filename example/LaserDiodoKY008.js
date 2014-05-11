19a910b44f73dec6e8fcd4042ac1216a52c19381
Julio C. Rodriguez Dominguez <jcrodriguez@aserta.com.mx>
2014-05-11 18:10:11 -0500

    Ejemplo para el módulo KY-008 Laser Diode

---
+++ example/LaserDiodoKY008.js
@@ -0,0 +1,22 @@
+var five = require("johnny-five"),
+    board = new five.Board(),
+    laserDiode;
+
+  board.on("ready", function() {
+
+  laserDiode = new five.Led({
+    pin: 8
+  });
+
+  // Hacemos que este disponible el objeto "laserDiodo" dentro del REPL
+  this.repl.inject({
+      laser: laserDiode
+  });
+
+  console.log( 'LD ready!' );
+
+  laser.on()
+
+  // Dentro del REPL, puedes probar el PWM con: laser.pulse() 
+
+});
