521f4e6bb6dd6d2c7152fb13d83bbf47be54d13f
Julio C. Rodriguez Dominguez <jcrodriguez@aserta.com.mx>
2014-05-10 23:55:10 -0500

    Ejemplo para el módulo KY-018 Photoresistor

---
+++ example/PhotoresistorKY018.js
@@ -0,0 +1,18 @@
+var five = require("johnny-five"),
+    board, photoresistor;
+
+board = new five.Board();
+
+board.on("ready", function() {
+
+  // Creamos una instancia de Sensor, indicándole que lea con una frecuencia de 100 milisegundos.
+  photoresistor = new five.Sensor({
+    pin: "A2",
+    freq: 100
+  }); 
+
+  photoresistor.on("data", function() {
+    console.log(this.value);
+  });
+  
+});
